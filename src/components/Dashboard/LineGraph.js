1504a805be251d61db8db181a5f237c57cf5cfa0
Ryan Brockhoff <brockhoffrw@gmail.com>
2019-03-15 15:42:02 -0500

    feat: adding buttons to show dynamic data change.

--- src/components/Dashboard/LineGraph.js
+++ src/components/Dashboard/LineGraph.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Chart from "chart.js";
 import classes from "./LineGraph.module.css";
-
+var myChart;
 //--Chart Style Options--//
 Chart.defaults.global.defaultFontFamily = "'PT Sans', sans-serif"
 Chart.defaults.global.legend.display = false;
@@ -12,8 +12,17 @@ export default class LineGraph extends Component {
 
     componentDidMount() {
         const { data, average, labels } = this.props;
-        console.log(data.length, labels.length)
-        new Chart(this.chartRef.current, {
+        const myChartRef = this.chartRef.current.getContext('2d');
+        const { width: chartWidth } = myChartRef.canvas;
+
+        //---Gradient Styling---//
+        let gradientLine = myChartRef
+            .createLinearGradient(0, 0, chartWidth, 0);
+        gradientLine.addColorStop(0.7, "#98b9ab");
+        gradientLine.addColorStop(1, "#a698b9");
+        //---Gradient Styling---//
+
+        myChart = new Chart(myChartRef, {
             type: "line",
             data: {
                 //Bring in data
@@ -22,13 +31,15 @@ export default class LineGraph extends Component {
                     {
                         label: "Sales",
                         data: data,
-                        fill: false
+                        fill: false,
+                        borderColor: gradientLine
 
                     },
                     {
                         label: "National Average",
                         data: average,
-                        fill: false
+                        fill: false,
+                        borderColor: gradientLine
                     }
                 ]
             },
--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -4,15 +4,40 @@ import LineGraph from "../../components/Dashboard/LineGraph";
 import { managerData, nationalAverage, yearLabels } from "./mockData";
 
 export default class Dashboard extends Component {
+    state = {
+        data: managerData,
+        nationalAverageData: nationalAverage,
+        labels: yearLabels
+    }
+
+    handleButtonClick = e => {
+        const { value } = e.target;
+    }
 
     render() {
+        const { data, nationalAverageData, labels } = this.state;
         return (
             <div className={classes.container}>
                 <h1>Sales Dashboard</h1>
                 <LineGraph
-                    data={managerData}
-                    average={nationalAverage}
-                    labels={yearLabels} />
+                    data={data}
+                    average={nationalAverageData}
+                    labels={labels} />
+                <div className={classes.buttonContainer}>
+                    <button
+                        value="annual"
+                        onClick={this.handleButtonClick}
+                    >
+                        Annual
+                    </button>
+
+                    <button
+                        value="lastquarter"
+                        onClick={this.handleButtonClick}
+                    >
+                        Last Quarter
+                    </button>
+                </div>
             </div>
         )
     }
