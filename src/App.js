3dce493df1f59f7cb076648785187c59aaca97da
Ryan Brockhoff <brockhoffrw@gmail.com>
2019-03-14 23:12:52 -0500

    feat: added component structure.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
+import Dashboard from "./views/Dashboard/Dashboard";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Chart Demo</h1>
-      </div>
+      <Dashboard />
     );
   }
 }
---
+++ src/views/Dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import classes from "./Dashboard.module.css";
+
+export default class Dashboard extends Component {
+
+    render() {
+        return (
+            <div className={classes.container}>
+                Dashboard View
+            </div>
+        )
+    }
+}
