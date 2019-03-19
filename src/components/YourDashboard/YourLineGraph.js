d8fcdfad9a0e12ca12fb24dcc7da08eb5bcab1b7
Ryan Brockhoff <brockhoffrw@gmail.com>
2019-03-18 22:30:29 -0500

    chore: added instructions and details to pkg.json

--- src/components/YourDashboard/YourLineGraph.js
+++ src/components/YourDashboard/YourLineGraph.js
@@ -3,7 +3,7 @@ import classes from "./YourLineGraph.module.css";
 
 export default class YourLineGraph extends Component {
 
-    //You can view the walkthrough code at any time by referring to the gists in the Medium article, or the Dashboard.js and LineChart.js files. Some code in LineChart.js will not reflect all changes mentioned in the article.
+    //You can view the walkthrough code at any time by referring to the gists in the Medium article, or the Dashboard.js and LineChart.js files. Some code in LineChart.js will not reflect all changes mentioned in the article. Styling is automatically imported in both of your views. You can use my provided classes or write your own styles. ChartJS is already installed.
 
     render() {
         return (
