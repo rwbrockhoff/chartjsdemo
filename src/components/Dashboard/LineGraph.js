f353c6f8dfd4d3f355f1ecb7355d2d5611c112b1
Ryan Brockhoff <brockhoffrw@gmail.com>
2019-03-18 22:15:02 -0500

    chore: clean up lineGraph and mockData.

--- src/components/Dashboard/LineGraph.js
+++ src/components/Dashboard/LineGraph.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Chart from "chart.js";
 import classes from "./LineGraph.module.css";
-var myLineChart;
+let myLineChart;
 
 //--Chart Style Options--//
 Chart.defaults.global.defaultFontFamily = "'PT Sans', sans-serif"
@@ -22,7 +22,6 @@ export default class LineGraph extends Component {
     buildChart = () => {
         const myChartRef = this.chartRef.current.getContext("2d");
         const { data, average, labels } = this.props;
-        console.log(data, average)
 
         if (typeof myLineChart !== "undefined") myLineChart.destroy();
 
@@ -48,12 +47,13 @@ export default class LineGraph extends Component {
             },
             options: {
                 //Customize chart options
-
             }
         });
+
     }
 
     render() {
+
         return (
             <div className={classes.graphContainer}>
                 <canvas
--- src/views/Dashboard/mockData.js
+++ src/views/Dashboard/mockData.js
@@ -1,18 +1,3 @@
-export const districtManagerData = [
-    { newarkBranch: 4500, austinBranch: 3700, denverBranch: 4900 },
-    { newarkBranch: 3900, austinBranch: 3700, denverBranch: 4900 },
-    { newarkBranch: 3450, austinBranch: 3700, denverBranch: 4900 },
-    { newarkBranch: 4600, austinBranch: 3700, denverBranch: 4900 },
-    { newarkBranch: 4435, austinBranch: 3700, denverBranch: 4900 },
-    { newarkBranch: 4100, austinBranch: 3700, denverBranch: 4900 },
-    { newarkBranch: 5120, austinBranch: 3700, denverBranch: 4900 },
-    { newarkBranch: 4200, austinBranch: 3700, denverBranch: 4900 },
-    { newarkBranch: 4100, austinBranch: 3700, denverBranch: 4900 },
-    { newarkBranch: 4200, austinBranch: 3700, denverBranch: 4900 },
-    { newarkBranch: 3950, austinBranch: 3700, denverBranch: 4900 },
-    { newarkBranch: 3600, austinBranch: 3700, denverBranch: 4900 }
-];
-
 export const managerData = [
     3910, 3706, 2850, 4005, 3750, 2912, 3200, 3645, 4205, 3211, 3354, 3904
 ];
@@ -37,3 +22,21 @@ export const yearLabels = ["January", "February", "March", "April", "May", "June
 export const quarterLabels = ["Week 1", "Week 2", "Week 3", "Week 4", "Week 5", "Week 6", "Week 7", "Week 8", "Week 9", "Week 10", "Week 11", "Week 12"];
 
 //---Labels---//
+
+
+//---More Complex Dataset---//
+
+export const districtManagerData = [
+    { sum: 13100, newarkBranch: 4500, austinBranch: 3700, denverBranch: 4900 },
+    { sum: 12500, newarkBranch: 3900, austinBranch: 3700, denverBranch: 4900 },
+    { sum: 12050, newarkBranch: 3450, austinBranch: 3700, denverBranch: 4900 },
+    { sum: 13200, newarkBranch: 4600, austinBranch: 3700, denverBranch: 4900 },
+    { sum: 13035, newarkBranch: 4435, austinBranch: 3700, denverBranch: 4900 },
+    { sum: 12700, newarkBranch: 4100, austinBranch: 3700, denverBranch: 4900 },
+    { sum: 13720, newarkBranch: 5120, austinBranch: 3700, denverBranch: 4900 },
+    { sum: 13550, newarkBranch: 4700, austinBranch: 3950, denverBranch: 4900 },
+    { sum: 12700, newarkBranch: 4100, austinBranch: 3700, denverBranch: 4900 },
+    { sum: 12800, newarkBranch: 4200, austinBranch: 3700, denverBranch: 4900 },
+    { sum: 11800, newarkBranch: 3950, austinBranch: 3650, denverBranch: 4200 },
+    { sum: 12745, newarkBranch: 3620, austinBranch: 3780, denverBranch: 5345 }
+];
